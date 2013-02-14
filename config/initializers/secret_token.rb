a8027e9f480737f22b1fac43787f0820b8d36c8e
SJC <sarajoyculver@gmail.com>
2013-02-14 11:57:26 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoungWorkers::Application.config.secret_token = '079881075bf008909ffa1d67a298ae3718f08deb8f9ae7db9d38bdc2334b6ff71842b7f6eb80908f8b5e0fe30ba45e7a8f1220d03fac6d339cec012792a97e86'
