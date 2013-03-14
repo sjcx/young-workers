1a22c3191b38e659a8c638e9ce0568077c6db787
SJC <sarajoyculver@gmail.com>
2013-03-14 13:57:25 -0700

    added new badge to entries where created_at === today

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,13 @@
+//this guy makes the 'new' label show up on posts created_at today
 jQuery(function() {
 	$(document).ready(function() {
+		$('.today').hide();
 		var d = new Date();
 		var date = ("0" + d.getDate()).slice(-2);
 		var month = ("0" + (d.getMonth() + 1)).slice(-2);
-		var today = date + "-" + month + "-" + d.getFullYear();
-		console.log(today);
+		var today = String(date + "-" + month + "-" + d.getFullYear());
 		if (document.getElementById(today) !== null) {
-			$('.today').show();
-		} else
-			$('.today').hide();
+			$('.today').first().show();
+		} 
 	})
 })
